7bd620b3942b52f396cff9756b52a15e99f657a5
Joel Getzke <joelcode2023@gmail.com>
2023-08-06 22:36:16 -0500

    find all categories and find by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,30 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({include: [Product,]}).then(category =>
+    { 
+      
+      res.json(category)
+     
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {include: [{model: Product, }],}).then (category =>
+    {
+      res.json(category)
+})
+.catch((err) => {
+  console.log(err);
+  res.status(400).json(err);
+});
 });
 
 router.post('/', (req, res) => {
