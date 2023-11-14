a30cc845c1046dd6ed9694e571edd805f0ccd884
Joel Getzke <joelcode2023@gmail.com>
2023-11-13 20:26:04 -0600

    route added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,6 +111,21 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product
+  .destroy({
+    where: { id: req.params.id }
+  })
+  .then(product => {
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this unique id.' });
+      return;
+    }
+    res.json({ message: 'Product was deleted successfully.' });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
